5c8bed49fcc8ead01f28cc1e81e2712ea6684103
Denis H. Roldán <112777347+denisrold@users.noreply.github.com>
2023-08-16 18:21:23 -0300

    Reconstruyendo

--- eManagerMicro/employees-service/employeesService/src/main/java/com/example/demo/DemoApplication.java
+++ eManagerMicro/employees-service/employeesService/src/main/java/com/example/demo/DemoApplication.java
@@ -1,6 +1,6 @@
 package com.example.demo;
 
-import com.example.demo.entities.Customer;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
